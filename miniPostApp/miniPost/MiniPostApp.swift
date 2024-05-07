3851d79f312b1a6c8c00456249f8c1624b47e5f7
Víctor Barrios <sygmaz19@gmail.com>
2024-05-07 18:41:22 +0200

    [refactor] Remove Legacy Profile

--- miniPostApp/miniPost/MiniPostApp.swift
+++ miniPostApp/miniPost/MiniPostApp.swift
@@ -7,30 +7,11 @@
 
 import SwiftUI
 
-class AppModules {
-    static let modules = AppModules()
-    
-    public var profileModule: ProfilePresenter?
-    public var modulesLoaded: Bool = false
-    
-    init() {
-        profileModule = createProfileModule()
-        modulesLoaded = true
-    }
-}
-
 @main
 struct MiniPostApp: App {
     var body: some Scene {
         WindowGroup {
-            // TODO: Review this initialisation point.
-            // It migth be a good idea to create an AppDelegate if we need this data loaded beforehand.
-            
-            if AppModules.modules.modulesLoaded {
-                MainMenuView()
-            } else {
-                Text("Loading...")
-            }
+            MainMenuView()
         }
     }
 }
